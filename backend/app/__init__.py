be7515a8729ba12650520ee169cf901545d4b00c
Brandibrandy <brandigathara@gmail.com>
2026-09-05 16:04:10 +0300

    wwww

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,19 @@
-﻿from flask import Flask
+﻿from flask import Flask, send_from_directory
 from flask_cors import CORS
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     CORS(app)
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path and os.path.exists(os.path.join(app.static_folder, path)):
+            return send_from_directory(app.static_folder, path)
+        return send_from_directory(app.static_folder, 'index.html')
+
     from .routes import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
+
     return app
